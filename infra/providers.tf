5f7869ecfbc623fcf53fceedc727b0ed8e6f4c3d
esraa <esraaessam122002@gmail.com>
2026-06-15 20:03:52 +0300

    second step

--- infra/providers.tf
+++ infra/providers.tf
@@ -7,6 +7,17 @@ terraform {
     bucket = "digilians-tfstate"
     key = "digilians,tfstate"
     region = "eu-west-1"
-    dynamodb_table = "YOUR_DYNAMODB_TABLE_NAME" // the denamodb name should write 
+    dynamodb_table = "esraa_trerraform_locks_digilians" 
   } 
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "esraa_trerraform_locks_digilians"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
