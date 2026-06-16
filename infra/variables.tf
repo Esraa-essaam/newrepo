99033e391af79b2525856aaad6f40a26729d2030
esraa <esraaessam122002@gmail.com>
2026-06-16 18:26:10 +0300

    terraform -apply

--- infra/variables.tf
+++ infra/variables.tf
@@ -7,3 +7,13 @@ variable "vpc_name" {
   type    = string
   default = "digilians-lnb"
 }
+variable "instance_type" {
+  description = "EC2 Instance Type"
+  type        = string
+  default     = "t2.micro"
+}
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "eu-west-1"
+}
