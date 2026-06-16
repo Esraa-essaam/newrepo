cead9481d8cce3c3c7cf14447958dc481c035321
esraa <esraaessam122002@gmail.com>
2026-06-16 18:42:13 +0300

    add

--- infra/providers.tf
+++ infra/providers.tf
@@ -9,3 +9,13 @@ terraform {
     region = "eu-west-1"
   }
 }
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
